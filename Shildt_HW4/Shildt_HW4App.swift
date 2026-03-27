b58f76884950bc64635fdaddca7ecac7559a233b
Greg Shildt <gshildt@okstate.edu>
2026-03-27 15:36:12 -0500

    Initial Commit

---
+++ Shildt_HW4/Shildt_HW4App.swift
@@ -0,0 +1,17 @@
+//
+//  Shildt_HW4App.swift
+//  Shildt_HW4
+//
+//  Created by Greg Shildt on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Shildt_HW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
